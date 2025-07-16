196a3b87f75d06a6143741f8b70b7f6673527325
ChamodMullegama <chamod.mullegama@gmail.com>
2025-07-17 01:41:17 +0530

    add home page

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\PersonalityController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Main route
+Route::get('/', [PersonalityController::class, 'showForm'])->name('personality.form');
