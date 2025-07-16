2a828e513fd40fa30c64d255984a70d1ff968c01
ChamodMullegama <chamod.mullegama@gmail.com>
2025-07-17 01:49:00 +0530

    add login page

--- app/Http/Controllers/AuthControllerr.php
+++ app/Http/Controllers/AuthControllerr.php
@@ -6,7 +6,13 @@ use Illuminate\Http\Request;
 
 class AuthControllerr extends Controller
 {
-        public function showRegister()
+
+    public function showLogin()
+    {
+        return view('Pages.Auth.login');
+    }
+
+    public function showRegister()
     {
         return view('Pages.Auth.register');
     }
